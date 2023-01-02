ed2614b76d2bb1ef5c29f49d13b65d0a0dd52875
hyeon woo <k99065633@gmail.com>
2023-01-02 22:17:37 +0900

    23.01.02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import { useState } from "react";
+import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import logo from "./logo.svg";
+import "./App.css";
+import "./test.css";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
+  console.log(shoes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          {shoes.map(function (a, i) {
+            return (
+              <Col md={4}>
+                <img src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`} />
+                <h4>{shoes[i].title}</h4>
+                <p>{shoes[i].price.toLocaleString()}</p>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
